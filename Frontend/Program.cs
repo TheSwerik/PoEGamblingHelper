f9e444fe5cee127bc250f3c9a39a28e5e2e50731
TheSwerik <derswerik@gmail.com>
2022-12-14 17:39:36 +0100

    add appsettings

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -9,14 +9,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-#if DEBUG
-const string backendUrl = "https://localhost:7166/";
-#endif
-#if RELEASE
-const string backendUrl = builder.HostEnvironment.BaseAddress + "api";
-#endif
-
-builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(backendUrl) });
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.Configuration["BackendUrl"]!) });
 builder.Services.AddScoped<IGemService>(sp => new GemService(sp.GetService<HttpClient>()!));
 builder.Services.AddBlazoredToast();
 builder.Services.AddBlazoredSessionStorage();
