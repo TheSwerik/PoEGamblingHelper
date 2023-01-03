e70e2265d5f7121ac7a70c98f27afc92dba4234b
Swerik <derswerik@gmail.com>
2023-01-03 01:55:10 +0100

    add Currency Conversion

--- Model/ExtensionMethods.cs
+++ Model/ExtensionMethods.cs
@@ -3,4 +3,9 @@
 public static class ExtensionMethods
 {
     public static int LevelModifier(this ResultCase resultCase) { return (int)resultCase - 1; }
+
+    public static bool EqualsIgnoreCase(this string a, string? b)
+    {
+        return a.Equals(b, StringComparison.InvariantCultureIgnoreCase);
+    }
 }
