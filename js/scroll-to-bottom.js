286242a8bdba115ec477639012995841ff1032ad
TheSwerik <47034061+TheSwerik@users.noreply.github.com>
2023-02-06 19:26:23 +0000

    Deploying to gh-pages from  @ 9d4197fd9a90b59c48f10e10b2d95de100d3cbdc 🚀

---
+++ js/scroll-to-bottom.js
@@ -0,0 +1,7 @@
+﻿window.onscroll = () => {
+    if (window.scrollInfoService != null)
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight)
+            window.scrollInfoService.invokeMethodAsync('OnScrollToBottom', window.scrollY);
+}
+window.RegisterScrollInfoService = scrollInfoService => window.scrollInfoService = scrollInfoService;
+window.UnRegisterScrollInfoService = () => window.scrollInfoService = undefined;
