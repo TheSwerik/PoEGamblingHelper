900c18777bfe616f3537441881497b5192dc16aa
Swerik <derswerik@gmail.com>
2023-02-17 15:05:23 +0100

    add icons and javascript

---
+++ src/Web/wwwroot/js/tooltip.js
@@ -0,0 +1,6 @@
+﻿// noinspection JSUnusedGlobalSymbols,JSUnusedLocalSymbols
+
+function addTooltips() {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerElement => new bootstrap.Tooltip(tooltipTriggerElement));
+}
