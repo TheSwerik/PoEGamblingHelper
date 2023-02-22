079be47ff3130d0d7f0d47750810153a4adf3951
Swerik <derswerik@gmail.com>
2023-02-22 23:02:55 +0100

    add Infrastructure Test

--- test/Infrastructure.Test/Services/DataFetchServiceTest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Infrastructure.Test.Services;
-
-public class DataFetchServiceTest
-{
-    [Fact] public void FetchCurrentLeagueTest() { Assert.Fail("Not Implemented"); }
-    [Fact] public void FetchCurrencyDataTest() { Assert.Fail("Not Implemented"); }
-    [Fact] public void FetchTemplePriceDataTest() { Assert.Fail("Not Implemented"); }
-    [Fact] public void FetchGemPriceDataTest() { Assert.Fail("Not Implemented"); }
-}
--- test/Infrastructure.Test/Util/ExtensionMethodsTest.cs
+++ test/Infrastructure.Test/Util/ExtensionMethodsTest.cs
@@ -1,8 +1,98 @@
-﻿namespace Infrastructure.Test.Util;
+﻿using Domain.Entity.Gem;
+using FluentAssertions;
+using Infrastructure.Services.FetchDtos;
+using Infrastructure.Util;
+
+namespace Infrastructure.Test.Util;
 
 public class ExtensionMethodsTest
 {
-    [Fact] public void EqualsIgnoreCaseTest() { Assert.Fail("Not Implemented"); }
-    [Fact] public void ToGemDataTest() { Assert.Fail("Not Implemented"); }
-    [Fact] public void ToGemDataWithExistingGemDataTest() { Assert.Fail("Not Implemented"); }
+    [Theory]
+    [InlineData("test", "test", true)]
+    [InlineData("tEst", "teSt", true)]
+    [InlineData("TEST", "TEST", true)]
+    [InlineData("testa", "test", false)]
+    [InlineData(null, "test", false)]
+    [InlineData("test", null, false)]
+    [InlineData(null, null, true)]
+    public void EqualsIgnoreCaseTest(string? a, string? b, bool isTrue)
+    {
+        a.EqualsIgnoreCase(b).Should().Be(isTrue);
+        b.EqualsIgnoreCase(a).Should().Be(isTrue);
+        a.EqualsIgnoreCase(a).Should().BeTrue();
+        b.EqualsIgnoreCase(b).Should().BeTrue();
+    }
+
+    [Fact]
+    public void ToGemDataTest()
+    {
+        const string empower = "Empower Support";
+        const string enlighten = "Enlighten Support";
+        const string empowerIcon = "Empower Icon";
+        const string enlightenIcon = "Enlighten Icon";
+        var list = new List<PoeNinjaGemData>
+                   {
+                       new() { Name = empower, Icon = empowerIcon },
+                       new() { Name = enlighten, Icon = enlightenIcon },
+                       new() { Name = empower, Icon = empowerIcon },
+                       new() { Name = empower, Icon = empowerIcon },
+                       new() { Name = enlighten, Icon = enlightenIcon }
+                   };
+        var group = list.GroupBy(p => p.Name).First();
+        var gemTradeData = new List<GemTradeData>
+                           {
+                               new() { Name = empower },
+                               new() { Name = empower },
+                               new() { Name = enlighten },
+                               new() { Name = empower },
+                               new() { Name = enlighten },
+                               new() { Name = enlighten }
+                           };
+
+        var result = group.ToGemData(gemTradeData);
+
+        result.Id.Should().Be(Guid.Empty);
+        result.Name.Should().Be(empower);
+        result.Icon.Should().Be(empowerIcon);
+        result.Gems.Should().BeEquivalentTo(gemTradeData.Where(g => g.Name.EqualsIgnoreCase(empower)));
+    }
+
+    [Fact]
+    public void ToGemDataWithExistingGemDataTest()
+    {
+        const string empower = "Empower Support";
+        const string enlighten = "Enlighten Support";
+        const string empowerIcon = "Empower Icon";
+        const string enlightenIcon = "Enlighten Icon";
+        var list = new List<PoeNinjaGemData>
+                   {
+                       new() { Name = empower, Icon = empowerIcon },
+                       new() { Name = enlighten, Icon = enlightenIcon },
+                       new() { Name = empower, Icon = empowerIcon },
+                       new() { Name = empower, Icon = empowerIcon },
+                       new() { Name = enlighten, Icon = enlightenIcon }
+                   };
+        var group = list.GroupBy(p => p.Name).First();
+        var gemTradeData = new List<GemTradeData>
+                           {
+                               new() { Name = empower },
+                               new() { Name = empower },
+                               new() { Name = enlighten },
+                               new() { Name = empower },
+                               new() { Name = enlighten },
+                               new() { Name = enlighten }
+                           };
+        var existingGemData = new List<GemData>
+                              {
+                                  new() { Name = enlighten, Id = Guid.Parse("00000000-0000-0000-0000-000000000002") },
+                                  new() { Name = empower, Id = Guid.Parse("00000000-0000-0000-0000-000000000001") }
+                              };
+
+        var result = group.ToGemData(gemTradeData, existingGemData);
+
+        result.Id.Should().Be(Guid.Parse("00000000-0000-0000-0000-000000000001"));
+        result.Name.Should().Be(empower);
+        result.Icon.Should().Be(empowerIcon);
+        result.Gems.Should().BeEquivalentTo(gemTradeData.Where(g => g.Name.EqualsIgnoreCase(empower)));
+    }
 }
