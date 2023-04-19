39b8664b7e330939dd20f7769ecc084a939d32b8
Swerik <derswerik@gmail.com>
2023-04-19 22:24:32 +0200

    fix Unit Tests

--- src/Domain/Entity/Analytics/View.cs
+++ src/Domain/Entity/Analytics/View.cs
@@ -4,6 +4,6 @@ namespace Domain.Entity.Analytics;
 
 public class View
 {
-    [Key] public byte[] IpHash { get; set; }
+    [Key] public byte[] IpHash { get; set; } = null!;
     public DateOnly TimeStamp { get; set; }
 }
