78cb5385fafd1ebfc6ec152290791c5d89db6ef4
Swerik <derswerik@gmail.com>
2023-04-22 04:35:28 +0200

    cleanup

--- src/Api/Controllers/HttpUtilFunctions.cs
+++ src/Api/Controllers/HttpUtilFunctions.cs
@@ -5,10 +5,8 @@ public static class HttpUtilFunctions
     public static string? GetRealIpAddress(this HttpRequest httpRequest)
     {
         var forwardedChain = httpRequest.HttpContext.Request.Headers["X-Forwarded-For"].First();
-        Console.WriteLine(forwardedChain);
         if (string.IsNullOrEmpty(forwardedChain)) return httpRequest.HttpContext.Connection.RemoteIpAddress?.ToString();
         var clientIp = forwardedChain.Split(',').First();
-        Console.WriteLine(clientIp);
         return clientIp;
     }
 }
