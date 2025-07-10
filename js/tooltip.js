550977a049c64bbee50553133730140ce2bc5171
TheSwerik <47034061+TheSwerik@users.noreply.github.com>
2025-07-10 12:41:35 +0000

    Deploying to gh-pages from  @ 3d842550f0fa3bef7e408ce2c9c436b9be3e7b28 🚀

--- js/tooltip.js
+++ js/tooltip.js
@@ -1,6 +1,6 @@
-﻿// noinspection JSUnusedGlobalSymbols
+﻿// noinspection JSUnusedGlobalSymbols,JSUnusedLocalSymbols
+
 function addTooltips() {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-    // noinspection JSUnusedLocalSymbols
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerElement => new bootstrap.Tooltip(tooltipTriggerElement));
 }
